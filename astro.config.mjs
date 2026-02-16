6d75f53de06116b0a57323d7fa041848116705cb
Martin Hintzmann Jürs <martin@hintzmann.dk>
2026-02-16 07:36:51 +0100

    Optimized website

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,27 @@
 import node from '@astrojs/node';
 import { defineConfig } from 'astro/config';
 
+// const { SITE_BASE } = import.meta.process.env.NODE_ENV, process.cwd(), '')
+const SITE_BASE = "/astro-store";
+
 // https://astro.build/config
 export default defineConfig({
-	prefetch: true,
+	// prefetch: true,
 	output: 'server',
+
+	site: 'https://hintzmann.github.io',
+	base: SITE_BASE || '',
+
 	adapter: node({
 		mode: 'standalone',
-	})
-});
+	}),
 
+	image: {
+		domains: ["localhost"],
+		remotePatterns: [{ protocol: "https" }]
+	},
+
+	build: {
+		inlineStylesheets: 'always'
+	}
+});
--- src/types/astro-cart.d.ts
+++ src/types/astro-cart.d.ts
@@ -10,6 +10,7 @@ declare global {
   interface HTMLAstroCartElement extends HTMLElement {
     add(item: AstroCartItem): void;
     remove(id: string | number): void;
+    removeItem(id: string | number): void;
     clear(): void;
     get(): AstroCartItem[];
     count(): number;
